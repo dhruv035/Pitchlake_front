0dd17be3bd0aa702c8bda3073a6e6db025f1532c
Dhruv Gupta <dhruv035@gmail.com>
2024-07-30 03:37:20 +0530

    Add read hooks

--- src/hooks/useOptionRound.tsx
+++ src/hooks/useOptionRound.tsx
@@ -1,60 +1,143 @@
 import { optionRoundABI } from "@/abi";
 import { PlaceBidArgs, RefundableBidsArgs, UpdateBidArgs } from "@/lib/types";
-import { useContract } from "@starknet-react/core";
+import { useAccount, useContract, useContractRead } from "@starknet-react/core";
+import { useCallback, useMemo } from "react";
 import { LibraryError } from "starknet";
 
 const useOptionRound = ({ address }: { address: string }) => {
-  const typedContract = useContract({
-    abi: optionRoundABI,
-    address,
-  }).contract?.typedv2(optionRoundABI);
+  const contractData = useMemo(() => {
+    return { abi: optionRoundABI, address };
+  }, [address]);
+  const { account } = useAccount();
+  const typedContract = useMemo(
+    () =>
+      useContract({
+        ...contractData,
+      }).contract?.typedv2(optionRoundABI),
+    [address]
+  );
 
+  //Read States
+  const { data: reservePrice } = useMemo(
+    () =>
+      useContractRead({ ...contractData, functionName: "get_reserve_price" }),
+    [typedContract]
+  );
 
-  const placeBid = async ({ amount, price }: PlaceBidArgs) => {
-    if (!typedContract) return;
-    try {
-      const res = await typedContract.place_bid(amount, price);
-      //process res.transaction hash
-    } catch (err) {
-      const error = err as LibraryError;
-      //Notify Error error.name and error.msg
-    }
-  };
+  const { data: strikePrice } = useMemo(
+    () =>
+      useContractRead({ ...contractData, functionName: "get_strike_price" }),
+    [address]
+  );
 
-  const updateBid = async ({ bidId, amount, price }: UpdateBidArgs) => {
-    if (!typedContract) return;
-    try {
-      const res = await typedContract.update_bid(bidId, amount, price);
-      //process res.transaction hash
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const biddingNonce = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_bidding_nonce_for",
+        args: [account?.address as string],
+      }),
+    [account?.address, contractData]
+  );
 
-  const refundUnusedBids = async ({ optionBuyer }: RefundableBidsArgs) => {
-    if (!typedContract) return;
-    try {
-      const res = await typedContract.refund_unused_bids(optionBuyer);
-      //process res.transaction hash
-    } catch (err) {
-      const error = err as LibraryError;
-      //Notify Error error.name and error.msg
-    }
-  };
+  const { data: bids } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_bids_for",
+        args: [account?.address as string],
+      }),
+    [account?.address, contractData]
+  );
 
-  const tokenizeOptions = async () => {
-    
+  const { data: refundableBids } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_refundable_bids_for",
+        args: [account?.address as string],
+      }),
+    [account?.address, contractData]
+  );
+
+  const { data: optionsBalance } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_total_options_balance_for",
+        args: [account?.address as string],
+      }),
+    [account?.address, contractData]
+  );
+
+  const { data: tokenizableOptions } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_tokenizable_options_for",
+        args: [account?.address as string],
+      }),
+    [account?.address, contractData]
+  );
+
+  //  const { data:  } = useMemo(
+  //   () =>
+  //     useContractRead({ ...contractData, functionName: "get_reserve_price" }),
+  //   [typedContract]
+  // );
+  //Write Calls
+  const placeBid = useCallback(
+    async ({ amount, price }: PlaceBidArgs) => {
+      if (!typedContract) return;
+      try {
+        const res = await typedContract.place_bid(amount, price);
+        //process res.transaction hash
+      } catch (err) {
+        const error = err as LibraryError;
+        //Notify Error error.name and error.msg
+      }
+    },
+    [typedContract]
+  );
+
+  const updateBid = useCallback(
+    async ({ bidId, amount, price }: UpdateBidArgs) => {
+      if (!typedContract) return;
+      try {
+        const res = await typedContract.update_bid(bidId, amount, price);
+        //process res.transaction hash
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [typedContract]
+  );
+
+  const refundUnusedBids = useCallback(
+    async ({ optionBuyer }: RefundableBidsArgs) => {
+      if (!typedContract) return;
+      try {
+        const res = await typedContract.refund_unused_bids(optionBuyer);
+        //process res.transaction hash
+      } catch (err) {
+        const error = err as LibraryError;
+        //Notify Error error.name and error.msg
+      }
+    },
+    [typedContract]
+  );
+
+  const tokenizeOptions = useCallback(async () => {
     if (!typedContract) return;
     try {
       const data = await typedContract.tokenize_options();
       //process res.transaction hash
-
     } catch (err) {
       //Notify Error error.name and error.msg
     }
-  };
+  }, [typedContract]);
 
-  const exerciseOptions = async () => {
+  const exerciseOptions = useCallback(async () => {
     if (!typedContract) return;
 
     try {
@@ -63,13 +146,22 @@ const useOptionRound = ({ address }: { address: string }) => {
     } catch (err) {
       //Notify Error error.name and error.msg
     }
-  };
-
-  
+  }, [typedContract]);
 
   return {
-    placeBid,updateBid,refundUnusedBids,tokenizeOptions,exerciseOptions
-  }
+    state: {
+      reservePrice,
+      strikePrice,
+      refundableBids,
+      tokenizableOptions,
+      biddingNonce,
+    },
+    placeBid,
+    updateBid,
+    refundUnusedBids,
+    tokenizeOptions,
+    exerciseOptions,
+  };
 };
 
 export default useOptionRound;
--- src/hooks/useVault.tsx
+++ src/hooks/useVault.tsx
@@ -1,50 +1,108 @@
-import {
-  useContract,
-} from "@starknet-react/core";
+import { useAccount, useContract, useContractRead } from "@starknet-react/core";
 import { vaultABI } from "@/abi";
 import { DepositArgs } from "@/lib/types";
-import {  LibraryError } from "starknet";
+import { LibraryError } from "starknet";
+import { use, useCallback, useMemo } from "react";
 
 const useVault = ({ address }: { address: string }) => {
-  const typedContract = useContract({
+  const contractData = {
     abi: vaultABI,
     address,
-  }).contract?.typedv2(vaultABI);
-
-  const depositLiquidity = async (depositArgs: DepositArgs) => {
-    if (!typedContract) {
-      //Throw toast here
-      return;
-    }
-    try {
-      const data = await typedContract.deposit_liquidity(
-        depositArgs.amount,
-        depositArgs.beneficiary
-      );
-      //Use data.transaction hash to watch for updates
-    } catch (err) {
-      const error = err as LibraryError;
-      //Throw toast with library error
-    }
   };
+  const typedContract = useMemo(
+    () =>
+      useContract({
+        ...contractData,
+      }).contract?.typedv2(vaultABI),
+    [address]
+  );
 
-  const withdrawLiquidity = async (withdrawAmount: number | bigint) => {
-    if (!typedContract) {
-      //Throw toast here
-      return;
-    }
-    try {
-      const data = await typedContract.withdraw_liquidity(withdrawAmount);
-      //Use data.transaction hash to watch for updates
-    } catch (err) {
-      const error = err as LibraryError;
-      //Throw toast with library error
-    }
-  };
+  const { account } = useAccount();
+
+  //Read States
+  const { data: lpLockedAmount } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_lp_locked_balance",
+        args: [account?.address as string],
+        watch: true,
+      }),
+    [account?.address]
+  );
+  const { data: lpUnlockedAmount } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_lp_unlocked_balance",
+        args: [account?.address as string],
+        watch: true,
+      }),
+    [account?.address]
+  );
+  const { data: vaultLockedAmount } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_total_locked_balance",
+        watch: true,
+      }),
+    [address]
+  );
+
+  const { data: vaultUnlockedAmount } = useMemo(
+    () =>
+      useContractRead({
+        ...contractData,
+        functionName: "get_total_unlocked_balance",
+        watch: true,
+      }),
+    [address]
+  );
+
+  //Write Calls
+  const depositLiquidity = useCallback(
+    async (depositArgs: DepositArgs) => {
+      if (!typedContract) {
+        //Throw toast here
+        return;
+      }
+      try {
+        const data = await typedContract.deposit_liquidity(
+          depositArgs.amount,
+          depositArgs.beneficiary
+        );
+        return data as { transaction_hash: string };
+        //Use data.transaction hash to watch for updates
+      } catch (err) {
+        const error = err as LibraryError;
+        //Throw toast with library error
+        return;
+      }
+    },
+    [typedContract]
+  );
+
+  const withdrawLiquidity = useCallback(
+    async (withdrawAmount: number | bigint) => {
+      if (!typedContract) {
+        //Throw toast here
+        return;
+      }
+      try {
+        const data = await typedContract.withdraw_liquidity(withdrawAmount);
+        //Use data.transaction hash to watch for updates
+      } catch (err) {
+        const error = err as LibraryError;
+        //Throw toast with library error
+      }
+    },
+    [typedContract]
+  );
 
   //State Transition
 
-  const startAuction = async () => {
+  const startAuction = useCallback(async () => {
     if (!typedContract) {
       //Throw toast here
       return;
@@ -56,9 +114,9 @@ const useVault = ({ address }: { address: string }) => {
       const error = err as LibraryError;
       //Throw toast with library error
     }
-  };
+  }, [typedContract]);
 
-  const endAuction = async () => {
+  const endAuction = useCallback(async () => {
     if (!typedContract) {
       //Throw toast here
       return;
@@ -70,9 +128,9 @@ const useVault = ({ address }: { address: string }) => {
       const error = err as LibraryError;
       //Throw toast with library error
     }
-  };
+  }, [typedContract]);
 
-  const settleOptionRound = async () => {
+  const settleOptionRound = useCallback(async () => {
     if (!typedContract) {
       //Throw toast here
       return;
@@ -84,10 +142,15 @@ const useVault = ({ address }: { address: string }) => {
       const error = err as LibraryError;
       //Throw toast with library error
     }
-  };
-  //Read States
+  }, [typedContract]);
 
   return {
+    state: {
+      vaultLockedAmount,
+      vaultUnlockedAmount,
+      lpLockedAmount,
+      lpUnlockedAmount,
+    },
     depositLiquidity,
     withdrawLiquidity,
     startAuction,
