4eb7c4a8607fe1ddd4d9c4c914a19cfd9dfdf331
Dhruv Gupta <dhruv035@gmail.com>
2024-08-07 22:35:36 +0530

    Cleaning

---
+++ src/components/OptionRound/OptionRound.tsx
@@ -0,0 +1,58 @@
+import {
+  OptionRoundActionsType,
+  OptionRoundState,
+  VaultState,
+} from "@/lib/types";
+import classes from "./OptionRound.module.css"
+import ExerciseOptions from "./OptionRoundActions/ExerciseOptions";
+import MintOptions from "./OptionRoundActions/MintOptions";
+import PlaceBid from "./OptionRoundActions/PlaceBid";
+import RefundBids from "./OptionRoundActions/RefundBids";
+import UpdateBid from "./OptionRoundActions/UpdateBid";
+
+export const OptionRound = ({
+  vaultState,
+  roundActions,
+  roundState,
+}: {
+  vaultState: VaultState;
+  roundActions: OptionRoundActionsType;
+  roundState: OptionRoundState;
+}) => {
+  return (
+    <div className={classes.container}>
+        <div>
+      <div>Round Id {roundState?.roundId?.toString()}</div>
+      <div>
+        <PlaceBid
+          vaultState={vaultState}
+          placeBid={roundActions.placeBid}
+          optionRoundState={roundState}
+        />
+        <UpdateBid
+          vaultState={vaultState}
+          updateBid={roundActions.updateBid}
+          optionRoundState={roundState}
+        />
+        <RefundBids
+          vaultState={vaultState}
+          refundBids={roundActions.refundUnusedBids}
+          optionRoundState={roundState}
+        />
+        <MintOptions
+          vaultState={vaultState}
+          mintOptions={roundActions.tokenizeOptions}
+          optionRoundState={roundState}
+        />
+        <ExerciseOptions
+          vaultState={vaultState}
+          exerciseOptions={roundActions.exerciseOptions}
+          optionRoundState={roundState}
+        />
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionRound
