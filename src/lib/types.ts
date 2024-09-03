e4b521076e8e657ca03de1c92556e322ad05a553
Sameer Kumar <sameer@nethermind.io>
2024-09-03 14:59:39 +0530

    updated types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -24,6 +24,7 @@ export enum RoundState {
   Running = 2, // Auction has ended, waiting for option round expiry date to settle
   Settled = 3,
 }
+
 export const RoundStateLabels: { [key in RoundState]: string } = {
   [RoundState.Open]: "Open",
   [RoundState.Auctioning]: "Auctioning",
@@ -46,19 +47,15 @@ export type VaultStateType = {
 };
 
 export type VaultActionsType = {
-  depositLiquidity: (
-    depositArgs: DepositArgs,
-  ) => Promise<void>;
-  withdrawLiquidity: (
-    withdrawArgs: WithdrawArgs,
-  ) => Promise<void>;
+  depositLiquidity: (depositArgs: DepositArgs) => Promise<void>;
+  withdrawLiquidity: (withdrawArgs: WithdrawArgs) => Promise<void>;
   startAuction: () => Promise<void>;
   endAuction: () => Promise<void>;
   settleOptionRound: () => Promise<void>;
 };
 
 export type OptionRoundStateType = {
-  address:string|undefined;
+  address: string | undefined;
   reservePrice: bigint | number | string;
   strikePrice: bigint | number | string;
   capLevel: bigint | number | string;
@@ -74,13 +71,62 @@ export type OptionRoundStateType = {
   optionSettleDate: Date;
 };
 
-export type OptionRoundActionsType = {
-  placeBid:(placeBids:PlaceBidArgs)=>Promise<void>,
-  updateBid:(updateBid:UpdateBidArgs)=>Promise<void>,
-  refundUnusedBids:(refundBids:RefundBidsArgs)=>Promise<void>,
-  tokenizeOptions:()=>Promise<void>,
-  exerciseOptions:()=>Promise<void>,
+export interface VaultDetailsProps {
+  vaultAddress: string;
+  status: RoundState;
+  strike: number;
+  tvl: number;
+  round: number;
+  timeLeft?: string;
+  capLevel: number;
+  roundID: number;
+  type: string;
+  fees: number;
+  apy?: number;
+  pnl?: number;
+  lastRoundPerf: number;
+  currRoundPerf?: number;
+  actions: string;
+  auctionStartDate: Date;
+  auctionEndDate: Date;
+  optionSettleDate?: Date;
 }
+
+export interface InfoItemProps {
+  label: string;
+  value: React.ReactNode;
+}
+
+export interface BalanceTooltipProps {
+  balance: {
+    locked: string;
+    unlocked: string;
+    stashed: string;
+  };
+}
+
+export type TabType = "Deposit" | "Withdraw" | "My Info";
+export type WithdrawTabType = "Liquidity" | "Queue" | "Collect";
+
+export interface SidePanelState {
+  activeTab: TabType;
+  activeWithdrawTab: WithdrawTabType;
+  amount: string;
+  isDepositAsBeneficiary: boolean;
+  showConfirmation: boolean;
+  showSuccess: boolean;
+  isButtonDisabled: boolean;
+  percentage: number;
+  queuedPercentage: number;
+}
+
+export type OptionRoundActionsType = {
+  placeBid: (placeBids: PlaceBidArgs) => Promise<void>;
+  updateBid: (updateBid: UpdateBidArgs) => Promise<void>;
+  refundUnusedBids: (refundBids: RefundBidsArgs) => Promise<void>;
+  tokenizeOptions: () => Promise<void>;
+  exerciseOptions: () => Promise<void>;
+};
 
 export type UpdateBidArgs = {
   bidId: string;
