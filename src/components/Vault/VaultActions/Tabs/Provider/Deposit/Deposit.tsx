3e7f2f4c9389240c03d6efb98e4b0c4cc3653096
Sameer Kumar <sameer@nethermind.io>
2024-09-05 00:06:04 +0530

    Deposit component for the provider

--- src/components/Vault/VaultActions/Tabs/Provider/Deposit/Deposit.tsx
+++ src/components/Vault/VaultActions/Tabs/Provider/Deposit/Deposit.tsx
@@ -1,147 +1,121 @@
-import { Button, InputNumber } from "antd";
-import buttons from "@/styles/Button.module.css";
-import classes from "./Deposit.module.css";
-import inputs from "@/styles/Input.module.css";
-import { useState } from "react";
-import { DepositArgs, VaultStateType } from "@/lib/types";
-import useERC20 from "@/hooks/erc20/useERC20";
+import React, { useState } from "react";
 import { useAccount } from "@starknet-react/core";
-import { stringToHex } from "@/lib/utils";
 import { useTransactionContext } from "@/context/TransactionProvider";
-
-import React from "react";
-import { SidePanelState } from "@/lib/types";
+import useERC20 from "@/hooks/erc20/useERC20";
+import { VaultStateType } from "@/lib/types";
 import InputField from "@/components/Vault/Utils/InputField";
-import ToggleSwitch from "@/components/Vault/Utils/ToggleSwitch";
+import { ChevronDown, User } from "lucide-react";
+import ActionButton from "@/components/Vault/Utils/ActionButton";
 
-interface DepositContentProps {
-  state: SidePanelState;
-  updateState: (updates: Partial<SidePanelState>) => void;
+interface DepositProps {
+  vaultState: VaultStateType;
+  showConfirmation: (amount: string, action: string) => void;
 }
 
-const DepositContent: React.FC<DepositContentProps> = ({
-  state,
-  updateState,
-}) => (
-  <>
-    <InputField
-      label="Enter Amount"
-      value={state.amount}
-      onChange={(e) => updateState({ amount: e.target.value })}
-      placeholder="e.g. 5"
-    />
-    <div className="flex justify-between text-sm mb-4">
-      <span className="text-gray-400">Unlocked Balance</span>
-      <span>34.8 ETH</span>
+const Deposit: React.FC<DepositProps> = ({ vaultState, showConfirmation }) => {
+  const [state, setState] = useState({
+    amount: "",
+    isDepositAsBeneficiary: false,
+    beneficiaryAddress: "",
+  });
+
+  const { account } = useAccount();
+  const { isDev, devAccount } = useTransactionContext();
+  const { balance } = useERC20(vaultState.ethAddress, vaultState.address);
+
+  const updateState = (updates: Partial<typeof state>) => {
+    setState((prevState) => ({ ...prevState, ...updates }));
+  };
+
+  const handleSubmit = () => {
+    showConfirmation(state.amount, "Deposit");
+  };
+
+  const isDepositDisabled = (): boolean => {
+    if (state.isDepositAsBeneficiary) {
+      return (
+        BigInt(state.amount) <= 0 || state.beneficiaryAddress.trim() === ""
+      );
+    }
+    return BigInt(state.amount) <= 0;
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-grow space-y-6">
+        <div className="flex space-x-2">
+          <button
+            className={`py-2 px-4 rounded border border-[#373632] ${
+              !state.isDepositAsBeneficiary
+                ? "bg-[#373632] text-[#F5EBB8]"
+                : "bg-[#121212] text-[#BFBFBF]"
+            }`}
+            onClick={() =>
+              updateState({
+                isDepositAsBeneficiary: false,
+                beneficiaryAddress: "",
+              })
+            }
+          >
+            For Myself
+          </button>
+          <button
+            className={`py-2 px-4 rounded border border-[#373632] ${
+              state.isDepositAsBeneficiary
+                ? "bg-[#373632] text-[#F5EBB8]"
+                : "bg-[#121212] text-[#BFBFBF]"
+            }`}
+            onClick={() => updateState({ isDepositAsBeneficiary: true })}
+          >
+            As Beneficiary
+          </button>
+        </div>
+
+        {state.isDepositAsBeneficiary && (
+          <div>
+            <InputField
+              type="text"
+              value={state.beneficiaryAddress}
+              label="Enter Address"
+              onChange={(e) =>
+                updateState({ beneficiaryAddress: e.target.value })
+              }
+              placeholder="Depositor's Address"
+              icon={
+                <User className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400" />
+              }
+            />
+          </div>
+        )}
+
+        <div>
+          <InputField
+            type="number"
+            value={state.amount}
+            label="Enter Amount"
+            onChange={(e) => updateState({ amount: e.target.value })}
+            placeholder="e.g. 5"
+            icon={
+              <ChevronDown className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400" />
+            }
+          />
+        </div>
+      </div>
+      <div className="mt-auto">
+        <div className="flex justify-between text-sm mb-4 pt-4">
+          <span className="text-gray-400">Unlocked Balance</span>
+          <span className="text-white">{balance.toString()} ETH</span>
+        </div>
+        <div className="flex justify-between text-sm mb-4 pt-4 border-t border-[#262626]">
+          <ActionButton
+            onClick={handleSubmit}
+            disabled={isDepositDisabled()}
+            text="Deposit"
+          />
+        </div>
+      </div>
     </div>
-    <ToggleSwitch
-      isChecked={state.isDepositAsBeneficiary}
-      onChange={() =>
-        updateState({ isDepositAsBeneficiary: !state.isDepositAsBeneficiary })
-      }
-      label="Deposit as Beneficiary"
-    />
-  </>
-);
+  );
+};
 
-export default DepositContent;
-
-// export default function Deposit({
-//   vaultState,
-//   deposit,
-// }: {
-//   vaultState: VaultStateType;
-//   deposit: (depositArgs: DepositArgs) => Promise<void>;
-// }) {
-//   const [amount, setAmount] = useState<string>("");
-//   const { account } = useAccount();
-//   const { isDev, devAccount } = useTransactionContext();
-//   const [displayInsufficientBalance, setDisplayInsufficientBalance] =
-//     useState<boolean>(false);
-//   const { balance, allowance, approve } = useERC20(
-//     vaultState.ethAddress,
-//     vaultState.address
-//   );
-
-//   const handleAmountChange = (value: string | null) => {
-//     if (value) setAmount(value);
-//     else setAmount("");
-//   };
-//   // const approveShares = async () => {
-//   //   const approveTx = await getERC20Instance(roundTokenData.depositsToken).populateTransaction.approve(
-//   //     vault.depositsController,
-//   //     roundTokenBalance
-//   //   );
-//   //   sendTransactionWithToasts(approveTx, () => setIsDepositClickable(true));
-//   // };
-
-//   //   const claim = async () => {
-//   //     const depositTx = await getDepositsControllerInstance(
-//   //       vault.depositsController
-//   //     ).populateTransaction.claimVaultShares(
-//   //       selectedRound,
-//   //       roundTokenBalance,
-//   //       account
-//   //     );
-//   //     sendTransactionWithToasts(depositTx);
-//   //   };
-
-//   return (
-//     <div className={classes.container}>
-//       <p className={classes.title}>{"Deposit"}</p>
-//       <div style={{ width: "100%" }}>
-//         {
-//           <>
-//             <InputNumber
-//               className={inputs.input}
-//               placeholder="Deposit Amount (ETH)"
-//               onChange={handleAmountChange}
-//               controls={false}
-//             />
-//             <div className={classes.controls}>
-//               <Button
-//                 style={{ flex: 1 }}
-//                 className={buttons.button}
-//                 title="approve"
-//                 disabled={false}
-//                 onClick={async () =>
-//                   await approve({
-//                     amount: BigInt(amount),
-//                     spender: vaultState.address,
-//                   })
-//                 }
-//               >
-//                 Approve
-//               </Button>
-//               <Button
-//                 style={{ flex: 1 }}
-//                 className={[buttons.button, buttons.confirm].join(" ")}
-//                 title="deposit"
-//                 disabled={
-//                   //!isDepositClickable || displayInsufficientBalance
-//                   false
-//                 }
-//                 onClick={async () => {
-//                   if (isDev) {
-//                     if (devAccount)
-//                       await deposit({
-//                         amount: BigInt(amount),
-//                         beneficiary: devAccount.address,
-//                       });
-//                   }
-//                   if (account)
-//                     await deposit({
-//                       amount: BigInt(amount),
-//                       beneficiary: account.address,
-//                     });
-//                 }}
-//               >
-//                 Deposit
-//               </Button>
-//             </div>
-//           </>
-//         }
-//       </div>
-//     </div>
-//   );
-// }
+export default Deposit;
