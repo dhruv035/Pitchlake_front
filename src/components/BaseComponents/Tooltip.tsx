fc9790bdde26b26fac410a903daffb5e4f1479ad
Sameer Kumar <sameer@nethermind.io>
2024-09-05 11:12:34 +0530

    font size changed to sm

--- src/components/BaseComponents/Tooltip.tsx
+++ src/components/BaseComponents/Tooltip.tsx
@@ -6,7 +6,7 @@ const BalanceTooltip: React.FC<BalanceTooltipProps> = ({
     <div className="relative group">
       {children}
       <div className="absolute bottom-full mb-2 hidden group-hover:block text-white p-4 rounded-md border border-[#262626] border-1 bg-[#121212] shadow-sm">
-        <h2 className="text-lg font-bold mb-2 border-b border-[#262626]">Balance Distribution</h2>
+        <h2 className="text-sm mb-2 border-b border-[#262626]">Balance Distribution</h2>
         <div className="space-y-2">
           <div className="flex justify-between">
             <span>Locked</span>
