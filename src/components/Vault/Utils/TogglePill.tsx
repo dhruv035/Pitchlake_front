1cb87ad4fa93fb96dbfa96007c5c9156edc45474
Sameer Kumar <sameer@nethermind.io>
2024-09-05 11:16:21 +0530

    adjusted according to the changed types names

--- src/components/Vault/Utils/TogglePill.tsx
+++ src/components/Vault/Utils/TogglePill.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useAtom } from "jotai";
 import { Database, UserCircle } from "lucide-react";
 import { vaultUserType } from "@/lib/atom/user-tab";
-import { VaultUserType } from "@/lib/types";
+import { VaultUserRole } from "@/lib/types";
 
 export const TogglePillButton = () => {
   const [vaultUser, setVaultUserType] = useAtom(vaultUserType);
@@ -11,22 +11,22 @@ export const TogglePillButton = () => {
     <div className="inline-flex rounded-md p-1 bg-[#0D0D0D] border border-[#262626]">
       <button
         className={`flex items-center px-4 py-2 rounded transition-colors duration-200 ease-in-out ${
-          vaultUser === VaultUserType.Provider
+          vaultUser === VaultUserRole.Provider
             ? "bg-[#373632] text-[#F5EBB8]"
             : "text-gray-300 hover:text-white"
         }`}
-        onClick={() => setVaultUserType(VaultUserType.Provider)}
+        onClick={() => setVaultUserType(VaultUserRole.Provider)}
       >
         <Database className="w-4 h-4 mr-2" />
         Provider
       </button>
       <button
         className={`flex items-center px-4 py-2 rounded transition-colors duration-200 ease-in-out ${
-          vaultUser === VaultUserType.Buyer
+          vaultUser === VaultUserRole.Buyer
             ? "bg-[#373632] text-[#F5EBB8]"
             : "text-gray-300 hover:text-white"
         }`}
-        onClick={() => setVaultUserType(VaultUserType.Buyer)}
+        onClick={() => setVaultUserType(VaultUserRole.Buyer)}
       >
         <UserCircle className="w-4 h-4 mr-2" />
         Buyer
