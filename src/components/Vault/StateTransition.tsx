7b76a8af6b02234ca60f5ef44ad954aac50e9ccf
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2024-11-22 17:28:01 -0500

    refine state transition component

--- src/components/Vault/StateTransition.tsx
+++ src/components/Vault/StateTransition.tsx
@@ -1,61 +1,16 @@
 import { useProtocolContext } from "@/context/ProtocolProvider";
 import { useProvider, useAccount } from "@starknet-react/core";
 import useLatestTimestamp from "@/hooks/chain/useLatestTimestamp";
-import {
-  LineChartDownIcon,
-  LineChartUpIcon,
-  BriefCaseIcon,
-} from "@/components/Icons";
-import { Clock, Cog } from "lucide-react";
-import { useMemo, useState, useEffect, useRef } from "react";
+import { useMemo, useState, useEffect } from "react";
 import useFossilStatus from "@/hooks/fossil/useFossilStatus";
 import { getDurationForRound, getTargetTimestampForRound } from "@/lib/utils";
 import { makeFossilCall } from "@/services/fossilRequest";
 import { useTransactionContext } from "@/context/TransactionProvider";
 import { num } from "starknet";
-import { StatusData } from "@/hooks/fossil/useFossilStatus";
-import { VaultStateType, OptionRoundStateType } from "@/lib/types";
-
-const getRoundState = ({
-  vaultState,
-  selectedRoundState,
-  fossilStatus,
-  fossilError,
-  pendingTx,
-}: {
-  vaultState: VaultStateType | undefined;
-  selectedRoundState: OptionRoundStateType | undefined;
-  fossilStatus: StatusData | null;
-  fossilError: string | null;
-  pendingTx: any;
-}) => {
-  //  console.log({ isAwaitingRoundStateUpdate });
-  if (!vaultState || !selectedRoundState) return "Settled";
-
-  const state = selectedRoundState.roundState.toString();
-  if (state === "Open" || state === "Auctioning" || state === "Settled")
-    return state;
-
-  const isFossilCompleted = fossilStatus?.status === "Completed";
-  const isFossilPending = fossilStatus?.status === "Pending";
-  const isFossilFailed = fossilStatus?.status === "Failed";
-  const isFossilReqFailed = fossilError !== null || fossilStatus === null;
-  // console.log({ isTransactionPending, isAwaitingRoundStateUpdate });
-
-  //if (pendingTx) return "Pending";
-  //if (isTransactionPending || isAwaitingRoundStateUpdate) return "Pending";
-
-  //if (prevRoundStateRef.current === roundState) return "Pending";
-
-  if (state === "Running") {
-    if (isFossilPending) return "Pending";
-    if (isFossilCompleted) return "Running";
-    if (isFossilFailed || !isFossilCompleted || isFossilReqFailed)
-      return "FossilReady";
-  }
-
-  return state;
-};
+import { useRoundState } from "@/hooks/stateTransition/useRoundState";
+import { useIsDisabled } from "@/hooks/stateTransition/useIsDisabled";
+import { getIconByRoundState } from "@/hooks/stateTransition/getIconByRoundState";
+import { useRoundPermissions } from "@/hooks/stateTransition/useRoundPermissions";
 
 const StateTransition = ({
   isPanelOpen,
@@ -65,92 +20,40 @@ const StateTransition = ({
   setModalState: any;
 }) => {
   const { vaultState, vaultActions, selectedRoundState } = useProtocolContext();
-  const { pendingTx, status } = useTransactionContext();
+  const { pendingTx } = useTransactionContext();
   const { account } = useAccount();
   const { provider } = useProvider();
   const { timestamp: timestampRaw } = useLatestTimestamp(provider);
   const timestamp = timestampRaw ? timestampRaw : "0";
+  const {
+    status: fossilStatus,
+    error: fossilError,
+    setStatusData: setFossilStatus,
+  } = useFossilStatus();
 
-  const { status: fossilStatus, error: fossilError } = useFossilStatus();
-
-  // State to track if we're awaiting round state update
   const [isAwaitingRoundStateUpdate, setIsAwaitingRoundStateUpdate] =
     useState(false);
 
-  const roundState = useMemo(() => {
-    return getRoundState({
-      vaultState,
-      selectedRoundState,
-      fossilStatus,
-      fossilError,
-      pendingTx,
-    });
-  }, [
-    vaultState,
+  const { roundState, prevRoundState } = useRoundState({
     selectedRoundState,
     fossilStatus,
     fossilError,
-    //pendingTx,
-    status,
-  ]);
-
-  // Previous roundState to detect changes
-  const prevRoundStateRef = useRef(roundState);
-
-  useEffect(() => {
-    // roundState has changed, reset the awaiting state
-    if (prevRoundStateRef.current !== roundState)
-      setIsAwaitingRoundStateUpdate(false);
-
-    prevRoundStateRef.current = roundState;
-  }, [selectedRoundState?.roundState, roundState]);
-
-  const canAuctionStart = useMemo(() => {
-    return (
-      num.toBigInt(timestamp) >= Number(selectedRoundState?.auctionStartDate)
-    );
-  }, [timestamp, selectedRoundState]);
-
-  const canAuctionEnd = useMemo(() => {
-    return (
-      num.toBigInt(timestamp) >= Number(selectedRoundState?.auctionEndDate)
-    );
-  }, [timestamp, selectedRoundState]);
-
-  const canRoundSettle = useMemo(() => {
-    return (
-      num.toBigInt(timestamp) >= Number(selectedRoundState?.optionSettleDate)
-    );
-  }, [timestamp, selectedRoundState]);
-
-  const canFossilRequest = useMemo(() => {
-    return (
-      num.toBigInt(timestamp) >= Number(selectedRoundState?.optionSettleDate)
-    );
-  }, [timestamp, selectedRoundState]);
-
-  const isDisabled = useMemo(() => {
-    if (!account) return true;
-    if (pendingTx) return true;
-    if (prevRoundStateRef.current !== roundState) return true;
-    if (roundState === "Pending") return true;
-    if (roundState === "FossilReady") return !canFossilRequest;
-    if (roundState === "Open") return !canAuctionStart;
-    if (roundState === "Auctioning") return !canAuctionEnd;
-    if (roundState === "Running") return !canRoundSettle;
-    return false;
-  }, [
-    account,
-    //pendingTx,
-    status,
+    pendingTx,
     isAwaitingRoundStateUpdate,
-    selectedRoundState,
-    roundState,
-    canFossilRequest,
+  });
+
+  const FOSSIL_DELAY = 15 * 60;
+
+  const {
     canAuctionStart,
     canAuctionEnd,
     canRoundSettle,
-  ]);
+    canSendFossilRequest,
+  } = useRoundPermissions(
+    timestamp.toString(),
+    selectedRoundState,
+    FOSSIL_DELAY,
+  );
 
   const actions: Record<string, string> = useMemo(
     () => ({
@@ -171,6 +74,7 @@ const StateTransition = ({
         clientAddress: vaultState?.fossilClientAddress,
         vaultAddress: vaultState?.address,
       });
+      setFossilStatus({ status: "Pending", error: undefined });
     } else if (roundState === "Open") {
       await vaultActions.startAuction();
     } else if (roundState === "Auctioning") {
@@ -178,6 +82,7 @@ const StateTransition = ({
     } else if (roundState === "Running") {
       await vaultActions.settleOptionRound();
     }
+    setIsAwaitingRoundStateUpdate(true);
 
     setModalState((prev: any) => ({
       ...prev,
@@ -185,27 +90,26 @@ const StateTransition = ({
     }));
   };
 
-  const getIcon = () => {
-    const stroke = isDisabled ? "var(--greyscale)" : "var(--primary)";
+  const isDisabled = useIsDisabled({
+    account,
+    pendingTx,
+    isAwaitingRoundStateUpdate,
+    roundState,
+    prevRoundState,
+    canSendFossilRequest,
+    canAuctionStart,
+    canAuctionEnd,
+    canRoundSettle,
+  });
 
-    if (roundState === "Pending") return <Clock className="w-4 h-4 ml-2" />;
-    if (roundState === "Open")
-      return <LineChartUpIcon classname="w-4 h-4 ml-2" stroke={stroke} />;
-    if (roundState === "Auctioning")
-      return <LineChartDownIcon classname="w-4 h-4 ml-2" stroke={stroke} />;
-    if (roundState === "FossilReady")
-      return <Cog className="w-4 h-4 ml-2" stroke={stroke} />;
-    if (roundState === "Running")
-      return (
-        <BriefCaseIcon
-          classname="w-4 h-4 ml-2"
-          fill="transparent"
-          stroke={stroke}
-        />
-      );
-  };
+  const icon = getIconByRoundState(roundState, isDisabled);
 
-  // Early returns for certain conditions
+  useEffect(() => {
+    if (prevRoundState !== roundState) {
+      setIsAwaitingRoundStateUpdate(false);
+    }
+  }, [roundState, prevRoundState]);
+
   if (!vaultState?.currentRoundId || !selectedRoundState || !vaultActions)
     return null;
 
@@ -239,11 +143,9 @@ const StateTransition = ({
           }}
         >
           <p>
-            {prevRoundStateRef.current !== roundState
-              ? "Pending"
-              : actions[roundState]}
+            {prevRoundState !== roundState ? "Pending" : actions[roundState]}
           </p>
-          {getIcon()}
+          {icon}
         </button>
       </div>
     </div>
